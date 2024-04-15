a13bd79711dda65d74ccbc6f4dd064398eef8a0e
Erick <erickfernandess2@gmail.com>
2024-04-15 14:45:30 -0500

    Initial features

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react"
 import Button from "./components/Button"
+import { useId } from "react"
 
 const App = () => {
+  const taskInputId = useId()
+
+  const [tasks, setTasks] = useState([])
+
+  const [newTask, setNewTask] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setTasks([...tasks, newTask])
+
+    setNewTask('')
+  }
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
   return (
     <>
       <h1>Todo List</h1>
 
-      <form>
+      <form onSubmit={ handleSubmit }>
         <input 
-          id="" 
+          id={ taskInputId }
           name=""
           placeholder="Task description" 
+          onChange={ handleChange }
           required
           type="text" 
+          value={ newTask }
         />
 
         <Button>Save</Button>
@@ -21,6 +43,10 @@ const App = () => {
         <li>Task 1</li>
         <li>Task 2</li>
         <li>Task 3</li>
+
+        { tasks.map((task) => (
+          <li>{ task }</li>
+        )) }
       </ul>
     </>
   )
